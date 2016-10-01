b66750a6608121aa22c2a7b17583b604afc161a4
Maartje Eyskens <meyskens@me.com>
2016-10-01 21:24:10 +0200

    Add SafariViewController and cordovaInAppBrowser

--- www/views/main/main.controller.js
+++ www/views/main/main.controller.js
@@ -1,8 +1,8 @@
 angular.module("player.controllers").controller("mainController", mainController)
 
-mainController.$inject = ["$scope", "configService", "colorsService", "$ionicPlatform", "$cordovaSocialSharing"]
+mainController.$inject = ["$scope", "configService", "colorsService", "$ionicPlatform", "$cordovaSocialSharing", "$cordovaInAppBrowser"]
 
-function mainController($scope, configService, colorsService, $ionicPlatform, $cordovaSocialSharing) {
+function mainController($scope, configService, colorsService, $ionicPlatform, $cordovaSocialSharing, $cordovaInAppBrowser) {
 
     // to do check connection
 
@@ -31,8 +31,30 @@ function mainController($scope, configService, colorsService, $ionicPlatform, $c
     }
 
     $scope.pressedButton = function (button) {
-        alert("Please open your browser and go to " + button.url);
-        alert("You seriously thought I coded this already???")
+        $ionicPlatform.ready(function () {
+            SafariViewController.isAvailable(function (available) {
+                if (available) {
+                    SafariViewController.show({
+                        url: button.url,
+                        hidden: false, // default false. You can use this to load cookies etc in the background (see issue #1 for details).
+                        animated: true, // default true, note that 'hide' will reuse this preference (the 'Done' button will always animate though)
+                        transition: "slide", // (this only works in iOS 9.1/9.2 and lower) unless animated is false you can choose from: curl, flip, fade, slide (default)
+                        enterReaderModeIfAvailable: false, // default false
+                        tintColor: $scope.config.tint, // default is ios blue
+                        barColor: $scope.shadedBackgroundColour, // on iOS 10+ you can change the background color as well
+                        controlTintColor: $scope.config.tint, // on iOS 10+ you can override the default tintColor
+                    })
+                } else {
+                    $cordovaInAppBrowser.open(button.url, "_blank", {
+                        location: "yes",
+                        clearcache: "no",
+                        toolbar: "yes",
+                    })
+                }
+            })
+        })
+        // alert("Please open your browser and go to " + button.url);
+        // alert("You seriously thought I coded this already???")
     }
 
     configService.getConfig().then(initialise); // to do handle error
